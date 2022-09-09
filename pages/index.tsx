00caad8eb892c278c79a67dd26a7e9ea636807c6
leofuna <leo.funa@gmail.com>
2022-09-09 08:57:31 -0300

    feat: session for verify auth user

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,19 @@
+import Header from 'components/core/Header';
 import type { NextPage } from 'next';
+import { useSession } from 'next-auth/react';
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
+  const session = useSession();
+  if (session.status !== 'authenticated') return <h1>Usuário não autenticado!</h1>;
   return (
     <div className={styles.container}>
       <Head>
         <title>Gestor de Milhas</title>
         <link rel='icon' href='/favicon.ico' />
       </Head>
+      <Header />
       Home
     </div>
   );
