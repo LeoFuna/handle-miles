6068053e0e3ae7375deed00bb99d975c354f8024
leofuna <leo.funa@gmail.com>
2022-09-12 08:50:08 -0300

    fix: menu modal handle

--- components/core/Header.tsx
+++ components/core/Header.tsx
@@ -1,9 +1,13 @@
 import { AppBar, Avatar, Box, Container, IconButton, Menu, MenuItem, Tooltip, Typography } from "@mui/material";
 import Image from "next/image";
-import { useState } from "react";
+import { useState, MouseEvent } from "react";
 
 function Header({ title = 'Visão Geral' }) {
-  const [openMenu, setOpenMenu] = useState(false);
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+
+  const handleOpenMenu = (event: MouseEvent<HTMLButtonElement>) => setAnchorEl(event.currentTarget);
+  const handleCloseMenu = () => setAnchorEl(null);
+  const open = Boolean(anchorEl);
 
   return (
     <AppBar position='static'>
@@ -11,15 +15,19 @@ function Header({ title = 'Visão Geral' }) {
         <Box width='30vw' sx={{ display: 'flex', justifyContent: 'start' }}>
           <Image width={50} height={50} alt='logo' src='/airplane-logo.png' />
         </Box>
+
         <Box width='40vw' sx={{ display: 'center', justifyContent: 'center' }}>
           <Typography variant='h4'>{ title }</Typography>
         </Box>
+
         <Box width='30vw' sx={{ display: 'flex', justifyContent: 'end' }}>
-          <IconButton onClick={() => setOpenMenu(!openMenu)}>
+          <IconButton onClick={handleOpenMenu}>
             <Image width={50} height={50} alt='logo' src='/airplane-logo.png' />
           </IconButton>
           <Menu
-            open={openMenu}
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleCloseMenu}
           >
             <MenuItem>
               <Typography>Teste</Typography>
