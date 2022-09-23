33976d5e0a192c595e4c66d5b2759b4b764c423e
leofuna <leo.funa@gmail.com>
2022-09-23 12:02:59 -0300

    fix: type for use transactions

--- hooks/transactions-hooks.ts
+++ hooks/transactions-hooks.ts
@@ -4,7 +4,25 @@ type TransactionsParams = {
   userId?: string,
 }
 
-export const useTransactions = (params: TransactionsParams) => {
+type Transaction = {
+  averagePrice: number,
+  company: string,
+  date: string,
+  id: string,
+  totalMiles: number,
+  type: string,
+  userId: string,
+}
+
+type TransactionsSWR = {
+  data?: {
+    transactions: Transaction[],
+  },
+  error?: any,
+  isValidating: boolean,
+}
+
+export const useTransactions = (params: TransactionsParams): TransactionsSWR => {
   const stringParams = new URLSearchParams(params);
   const transactions = useApi(`/api/transactions?${stringParams}`);
   return transactions;
