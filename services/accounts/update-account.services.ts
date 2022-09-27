bb5eba58dbb585e440932aae52f45a9959ef865b
leofuna <leo.funa@gmail.com>
2022-09-27 09:38:03 -0300

    feat: update account service

---
+++ services/accounts/update-account.services.ts
@@ -0,0 +1,61 @@
+import { db } from "db/firebase";
+import { collection, getDocs, updateDoc, doc, getDoc, where, query, DocumentData } from "firebase/firestore";
+
+type AccountFromStorage = {
+  id: string,
+  averagePrice: number,
+  companyId: string,
+  totalMiles: number,
+  userId: string,
+}
+
+type UpdateAccountPayload = {
+  companyId: string,
+  totalMiles: number,
+  averagePrice: number,
+  type: string,
+};
+
+type UpdateAccountResponse = {
+  id: string,
+}
+
+function calcNewAccountValues(type: string, totalMiles: number, averagePrice: number, account: AccountFromStorage | any): AccountFromStorage {
+  const newAccount = { ...account };
+  if (type === '+') {
+    const totalPrice = (newAccount.totalMiles / 1000) * newAccount.averagePrice + (totalMiles/1000) * averagePrice;
+    newAccount.totalMiles += totalMiles;
+    newAccount.averagePrice = totalPrice / (newAccount.totalMiles/1000);
+    return newAccount;
+  }
+  newAccount.totalMiles -= totalMiles;
+  return newAccount;
+}
+
+const updateAccountService = async (
+    userId: string,
+    { companyId, totalMiles, averagePrice, type }: UpdateAccountPayload
+  ): Promise<UpdateAccountResponse> => {
+  const accountsRef = collection(db, 'accounts');
+  const querySnapshot = query(accountsRef, where('userId', '==', `${userId}`), where('companyId', '==', `${companyId}`));
+  const documents = await getDocs(querySnapshot);
+  const [account] = documents.docs.map((doc) => {
+    const account = doc.data();
+    account.id = doc.id;
+    return account;
+  }); 
+
+  const newAccountValues = calcNewAccountValues(type, totalMiles, averagePrice, account);
+
+  const accountDocRef = doc(db, 'accounts', `${account.id}`);
+  await updateDoc(accountDocRef, {
+    averagePrice: newAccountValues.averagePrice,
+    totalMiles: newAccountValues.totalMiles,
+  });
+
+  return {
+    id: newAccountValues.id,
+  };
+};
+
+export default updateAccountService;
