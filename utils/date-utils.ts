6eb8b37af2d1b2bb4f5b759963d21df4c5f3befe
leofuna <leo.funa@gmail.com>
2022-09-27 09:31:58 -0300

    fix: date utils formateDate

--- utils/date-utils.ts
+++ utils/date-utils.ts
@@ -1,8 +1,5 @@
-import { fromUnixTime,format } from 'date-fns';
+import { format, parseISO } from 'date-fns';
 
-export function formatTimestampsToDate(timestampsSeconds: number, pattern: string ) {
-  const dateTime = fromUnixTime(timestampsSeconds);
-  const formatedDate = format(dateTime, pattern);
-
-  return formatedDate;
-} 
+export function formatDate(dateString: string, pattern: string ): string {
+  return format(parseISO(dateString), pattern);
+}
