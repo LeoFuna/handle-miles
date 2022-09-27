1158b282c2f7efaa73425d5ea497e43eb9f517e7
leofuna <leo.funa@gmail.com>
2022-09-27 09:37:50 -0300

    feat: create transaction route

--- pages/api/transactions/index.ts
+++ pages/api/transactions/index.ts
@@ -1,7 +1,8 @@
 import { db } from "db/firebase";
 import express from "express";
 import { collection, getDocs, query, where } from 'firebase/firestore';
-import { formatTimestampsToDate } from "utils/date-utils";
+import createTransactionService from "services/transactions/create-transaction.services";
+import { formatDate } from "utils/date-utils";
 
 const app = express();
 
@@ -11,12 +12,17 @@ app.get('/api/transactions', async (req: express.Request, res: express.Response)
   const querySnapshot = await getDocs(myQuery);
   const transactions = querySnapshot.docs.map((doc) => {
     const transaction = doc.data();
-    transaction.date = formatTimestampsToDate(transaction.date.seconds, 'dd/MM/yyyy');
+    transaction.date = formatDate(transaction.date, 'dd/MM/yyyy');
     transaction.id = doc.id;
     return transaction;
   });
 
   return res.json({ transactions });
+});
+
+app.post('/api/transactions', async (req: express.Request, res: express.Response) => {
+  const createResponse = await createTransactionService(JSON.parse(req.body));
+  return res.json(createResponse);
 });
 
 export default app;
