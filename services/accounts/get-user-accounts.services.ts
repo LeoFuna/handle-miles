4a366fe72a6b6d1a866c99ebd797eb023aeace22
leofuna <leo.funa@gmail.com>
2022-09-27 14:57:09 -0300

    fix: get user accounts service serialize company name

--- services/accounts/get-user-accounts.services.ts
+++ services/accounts/get-user-accounts.services.ts
@@ -1,15 +1,32 @@
 import { db } from "db/firebase";
-import { collection, getDocs, query, where } from "firebase/firestore";
+import { collection, DocumentData, getDocs, query, where } from "firebase/firestore";
+import listCompaniesService from "services/companies/list-companies.services";
 
-const getUserAccountsService = async (userId: any) => {
+type AccountResponse = {
+  averagePrice: number;
+  companyId: string;
+  totalMiles: number;
+  userId: string;
+  company: string;
+}
+
+type CompanyResponse = {
+  id: string;
+  name: string;
+}
+
+const getUserAccountsService = async (userId: any): Promise<AccountResponse[] | DocumentData> => {
+  const companies = await listCompaniesService();
   const accountsRef = collection(db, 'accounts');
   const querySnapshot = query(accountsRef, where('userId', '==', `${userId}`));
   const documentsAccounts = await getDocs(querySnapshot);
-  const accounts = documentsAccounts.docs.map((doc) => {
+  const accounts = documentsAccounts.docs.map((doc): AccountResponse | DocumentData => {
     const account = doc.data();
+    account.company = companies.find((company: CompanyResponse | DocumentData) => company.id === account.companyId)?.name;
     account.id = doc.id;
     return account;
   });
+
   return accounts;
 };
 
