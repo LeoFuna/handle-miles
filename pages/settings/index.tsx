3b9934a67e6d98d1fa214ab41dfab2d2e84b2feb
leofuna <leo.funa@gmail.com>
2022-10-10 16:24:47 -0300

    feat: settings page

---
+++ pages/settings/index.tsx
@@ -0,0 +1,13 @@
+import Header from "components/core/Header";
+import SettingsPanel from "components/settings/SettingsPanel";
+
+function Settings () {
+  return (
+    <>
+      <Header title='Configurações' />
+      <SettingsPanel />
+    </>
+  );
+}
+
+export default Settings;
