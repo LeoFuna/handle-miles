51d733b6c3fe76a2918e632149657722c7c3bca9
leofuna <leo.funa@gmail.com>
2022-10-13 17:38:04 -0300

    fix: user options to logout

--- components/core/Header.tsx
+++ components/core/Header.tsx
@@ -5,29 +5,33 @@ import { useState, MouseEvent } from "react";
 import { MenuBook } from "@mui/icons-material";
 import { signOut } from "next-auth/react";
 
-function Header({ title = 'Visão Geral' }) {
+function Header({ title = 'Visão Geral', name }: { title?: string; name: string }) {
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const [userAnchorEl, setUserAnchorEl] = useState<null | HTMLElement>(null);
   const router = useRouter();
 
   const handleOpenMenu = (event: MouseEvent<HTMLButtonElement>) => setAnchorEl(event.currentTarget);
+  const handleUserOpenMenu = (event: MouseEvent<HTMLButtonElement>) => setUserAnchorEl(event.currentTarget);
   const handleCloseMenu = () => setAnchorEl(null);
+  const handleUserCloseMenu = () => setUserAnchorEl(null);
   const open = Boolean(anchorEl);
+  const openUserMenu = Boolean(userAnchorEl);
 
   return (
     <AppBar position='static'>
-      <Box sx={{ height: '50px', display: 'flex', alignItems: 'center' }} >
-        <Box width='30vw' sx={{ display: 'flex', justifyContent: 'start' }}>
+      <Box sx={{ height: '70px', display: 'flex', alignItems: 'center' }} >
+        <Box width='30vw' m={5} sx={{ display: 'flex', justifyContent: 'start' }}>
           <Image width={50} height={50} alt='logo' src='/airplane-logo.png' />
         </Box>
 
         <Box width='40vw' sx={{ display: 'center', justifyContent: 'center' }}>
-          <Typography variant='h4'>{title}</Typography>
+          <Typography variant='h3'>{title}</Typography>
         </Box>
 
-        <Box width='30vw' sx={{ display: 'flex', justifyContent: 'end' }}>
+        <Box width='30vw' m={5} sx={{ display: 'flex', justifyContent: 'end' }}>
           <Tooltip title='Menu'>
-            <IconButton onClick={handleOpenMenu}>
-              <MenuBook />
+            <IconButton sx={{ marginRight: '30px' }} onClick={handleOpenMenu}>
+              <MenuBook fontSize='large' />
             </IconButton>
           </Tooltip>
           <Menu
@@ -46,10 +50,19 @@ function Header({ title = 'Visão Geral' }) {
             </MenuItem>
           </Menu>
           <Tooltip title='Usuário'>
-            <IconButton onClick={() => signOut()} sx={{ p: 0 }}>
-              <Avatar />
+            <IconButton onClick={handleUserOpenMenu} sx={{ p: 0 }}>
+              <Avatar>{name[0]}</Avatar>
             </IconButton>
           </Tooltip>
+          <Menu
+            anchorEl={userAnchorEl}
+            open={openUserMenu}
+            onClose={handleUserCloseMenu}
+          >
+            <MenuItem onClick={() => signOut()}>
+              <Typography>Sair</Typography>
+            </MenuItem>
+          </Menu>
         </Box>
       </Box>
     </AppBar>
