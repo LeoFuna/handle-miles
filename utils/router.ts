83690b9a4aacbe66e504060132b42cb9b49b63b7
leofuna <leo.funa@gmail.com>
2022-11-09 11:06:35 -0300

    chore: removing express dependecy and using next-connect for api server handling

---
+++ utils/router.ts
@@ -0,0 +1,18 @@
+import { createRouter } from "next-connect";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export const onError = (err: unknown, req: NextApiRequest, res: NextApiResponse) => {
+  let errorMessage = 'Erro Genérico';
+  if (err instanceof Error) {
+    errorMessage = err.message;
+  }
+  res.status(500).end(errorMessage);
+};
+
+export const onNoMatch = (req: NextApiRequest, res: NextApiResponse) => {
+  res.status(404).end("Page is not found");
+};
+
+const router = createRouter<NextApiRequest, NextApiResponse>();
+
+export default router;
