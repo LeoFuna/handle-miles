02e60986732d7f00b11861ede368c5e76ae28e18
leofuna <leo.funa@gmail.com>
2022-11-10 14:27:40 -0300

    feat: number utils

---
+++ utils/numbers-utils.ts
@@ -0,0 +1,9 @@
+export const formatPriceToPtBRCurrency = ({ value }: { value: number }) => {
+  const formatter = new Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+  });
+  return formatter.format(value).toString();
+};
+
+export const separeteNumberWithDots = ({value}: { value: number }) => value.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.');
